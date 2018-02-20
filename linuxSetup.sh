8310db1f92fd4f81418daced56c615f8c72e0253
Gang Cao <caogang1213@gmail.com>
2018-02-20 03:13:26 +0000

    Update linuxSetup.sh

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -34,29 +34,83 @@ sudo apt-get install zypper
 sudo zypper ref
 sudo zypper install -t parttern devel_C_C++ devel_kernel
 
-sudo zypper install wget
+sudo zypper install wget git cmake pkg-config
 
+#------------------------------
+# sudo
+sudo visudo -f /etc/sudoers.d/nopass
+# "gangcao ALL=(ALL) NOPASSWD:ALL"
+reboot
+
+#------------------------------
+# install nvidia graphic card driver
+sudo ln -s /usr/src/linux-$(uname -r)/include/generated/uapi/linux/version.h /usr/src/linux-$(uname -r)/include/linux/
 cd /home/gangcao/Desktop/
 sudo wget "http://us.download.nvidia.com/XFree86/Linux-x86_64/384.111/NVIDIA-Linux-x86_64-384.111.run"
 echo "blacklist nouveau" >> /etc/modprobe.d/50-blacklist.conf && mkinitrd
 
+# Ctrl + Alt + F1
+# login with user account, and su-
 init 3
-
-#------------------------------
-# install Qt
-
+cd /home/gangcao/Desktop/
+chmod 777 NVIDIA-Linux-x86_64-384.111.run
+./NVIDIA-Linux-x86_64-384.111.run
 
 #------------------------------
 # install cuda
+cd /home/gangcao/Download/
+chmod 777 cuda_8.0.61_375.26_linux.run
+./cuda_8.0.61_375.26_linux.run
+sudo touch /etc/ld.so.conf.d/cuda.conf
+echo "/usr/local/cuda/lib64" >> /etc/ld.so.conf.d/cuda.conf
+# For cuda
+echo "export PATH="/usr/local/cuda/bin:$PATH"" >> ~/.bashrc
+echo "export CUDA_PATH="/usr/local/cuda"" >> ~/.bashrc
 
 #------------------------------
 # install cudnn
+# install cuDNN v6.0
+cd /home/gangcao/Desktop
+wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/cudnn-8.0-linux-x64-v6.0.tgz
+tar -xzvf cudnn-8.0-linux-x64-v6.0.tgz"
+sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
+sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
+sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
+
+#------------------------------
+# install Qt
+sudo chmod +x qt-opensource-linux-x64-5.9.1.run && ./qt-opensource-linux-x64-5.9.1.run
+sudo touch /etc/ld.so.conf.d/qt.conf
+echo "/opt/Qt/lib" >> /etc/ld.so.conf.d/qt.conf
+sudo ln -s /opt/Qt5.9.1/5.9/gcc_64/ /opt/Qt
+sudo vim ~/.bashrc
+# For Qt 
+echo "export PATH="/opt/Qt/bin:$PATH"" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH="/opt/Qt/lib:$LD_LIBRARY_PATH"" >> ~/.bashrc
+
+#------------------------------
+# install scim-pinyin
+sudo zypper addrepo https://download.opensuse.org/repositories/M17N/openSUSE_Leap_42.2/M17N.repo
+sudo zypper refresh
+sudo zypper install scim-pinyin
+
+#------------------------------
+# install tensorflow 
+sudo apt-get install python-pip python-dev
+sudo python2.7 -m pip install --upgrade \
+  https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-1.3.0-cp27-none-linux_x86_64.whl
+
+#------------------------------
+# install python packages
+sudo apt-get install python-tk python-pyside
+sudo python2.7 -m pip install matplotlib lxml Pillow scipy
 
 #------------------------------
 # install opencv
 
 #------------------------------
 # install google chromium
+sudo apt-get install chromium-browser
 
 #------------------------------
 # install tex full
